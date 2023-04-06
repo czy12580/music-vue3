104d60a68f86d870274c5370d32529ad88a58f25
funny <funnychen@vectorbuilder.ne>
2023-04-06 17:23:46 +0800

    dist

--- dist/assets/index.289f6229.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./Header.0c4adbe6.js";function o(n){return t.post("/login/cellphone",n)}function r(n){return t.post("/register/cellphone",n)}function a(n){return t.get("/captcha/sent?phone="+n.phone)}function c(){return t.get("/logout")}function s(){return t.get("/banner?type=2")}export{c as a,a as c,s as g,o as l,r};
